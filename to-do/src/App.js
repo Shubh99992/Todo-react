170dfdcac6514a3a505f8f4925317ce0d3a5fd35
Shubham <105529358+Shubh99992@users.noreply.github.com>
2024-03-03 15:42:12 +0000

    adding feature

---
+++ to-do/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Todo from "./component/todo"
+const App = () => {
+  return (
+    <div>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
---
+++ to-do/src/component/todo.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import '../App.css'
+const Todo = () => {
+    const [allTodos, setTodos] = useState([]);
+    const [newTitle, setNewTitle] = useState('');
+    const [newDescription, setNewDescription] = useState('');
+
+    const handleAddTodo = () => {
+        let newTodoItem = {
+            title: newTitle,
+            description: newDescription
+        }
+        let updatedTodo = [...allTodos];
+        updatedTodo.push(newTodoItem);
+        setTodos(updatedTodo);
+    }
+   
+   
+
+  return (
+    <div>
+        <div className='container'>
+            <div id='box1'>
+                <label>Title</label>
+                <input type='text' id='title' value={newTitle} onChange={(e) => setNewTitle(e.target.value)} />
+            </div>
+            <div id='box2'>
+                <label>Description</label>
+                <input type='text' id='Description' value={newDescription} onChange={(e)=>setNewDescription(e.target.value)} />
+            </div>
+
+        </div>
+        <button id='add' onClick={handleAddTodo}>Add</button>
+        <div id='list'><h3>Todos</h3>
+           {allTodos.map((item,index)=>{
+            return(
+                <div id='list-item' style={{display: "flex"}} key={index}>
+                <div  >
+                    <h3>{item.title}</h3>
+                    <p>{item.description}</p>
+                    
+                </div>
+                <button style={{ border: "none"}} id='dlt' >❌</button>
+            </div>
+            )
+           })}
+        </div>
+    </div>
+  )
+}
+
+export default Todo
